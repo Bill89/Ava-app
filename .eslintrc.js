8316d0fcb0ef6d7718a9ebb06b325a4c3cf732c0
Victor <victor.dev.f1@gmail.com>
2019-06-04 21:20:34 +0800

    修改编辑器配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,11 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // 函数圆括号之前没有一个空格
+    'space-before-function-paren': ['error', 'never']
   },
   parserOptions: {
     parser: '@typescript-eslint/parser'
